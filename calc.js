80c99d9df7144e6aab9402c18980b6d46b9b5518
Yuchuk <yurachuluun@gmail.com>
2022-02-16 09:27:42 -0600

    2/16/2022
    string to int
    aldaa bsniig zasaad duusgalaa

--- calc.js
+++ calc.js
@@ -1,15 +1,7 @@
-function calculateTip(){
-	var numSubTotal = parseInt(document.getElementsByName("numSubTotal")[0]);
-	var numTipPercent = parseInt(document.getElementsByName("numTipPercent")[0]);
-	var totalAmount = parseInt(document.getElementsByName("totalAmount")[0]);
-		console.log(numSubTotal);
-		console.log(typeof(numSubTotal));
-		console.log(numTipPercent);
-		console.log(typeof(numTipPercent));
-		console.log(totalAmount);
-		console.log(typeof(totalAmount));
-	var numTipAmount = numSubTotal.value * numTipPercent.value[0]/100;
-	totalAmount.innerHTML = numSubTotal.value + numTipAmount;
-		console.log(totalAmount[0]);
-		console.log(typeof(totalAmount[0]));
+function calculateTip() {
+	var numSubTotal = parseInt((document.getElementsByName("numSubTotal")[0]).value);
+	var numTipPercent = parseInt((document.getElementsByName("numTipPercent")[0]).value);
+	var totalAmount = document.getElementsByName("totalAmount")[0];
+	var numTipAmount = numSubTotal * numTipPercent / 100;
+	totalAmount.innerHTML = numSubTotal + numTipAmount;
 }
